56b5686866f8ccc48f6f691a73c22a306f006e46
thaonk <taonuaa004@gmail.com>
2023-02-28 02:21:52 +0700

    add dt

---
+++ ReadAndWriteFileExcel/TrungTrang/DTO/HostConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrungTrang.DTO
+{
+    public class HostConfig
+    {
+        public string key;
+        public string name { get; set; }
+        public string host;
+        public string diaChi;
+
+        public HostConfig()
+        {
+
+        }
+
+        public HostConfig(string key, string name, string host, string diaChi)
+        {
+            this.key = key;
+            this.name = name;
+            this.host = host;
+            this.diaChi = diaChi;
+        }
+    }
+}
